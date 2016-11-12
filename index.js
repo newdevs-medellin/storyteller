f0f2bb47608ca009de4b6d6fe0c24dc2974f18e2
Jesse Cogollo <cogollo87@gmail.com>
2016-11-12 10:40:49 -0500

    separar express conf de index.js

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
 'use strict';
 const express = require('express');
 const app = express();
-const exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
-
-app.use('/assets', express.static('assets'));
+require('./config/express')(app);
 
 app.get('/', function(req, res) {
   res.render('hello');
 });
-
-app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
