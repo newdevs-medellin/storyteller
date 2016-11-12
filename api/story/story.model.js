06faed3fb45c3b30b3dd9e679755e7bed56eec57
Jesse cogollo <cogollo87@gmail.com>
2016-11-12 17:42:55 -0500

    Feature/schema mongodb (#15)
    * create story model and service
    
    * conf mongodb local to save and get stories
    
    * adding connection mongodb to 3party service mlab

---
+++ api/story/story.model.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// {"userId":"xxx","messageId":"yyy","sentence":"zzz","nouns":[{"noun": "one", "imgUrl":"imageOne"}, {"noun": "two", "imgUrl":"imageTwo"},{"noun": "three", "imgUrl":"imageThree"}]}
+
+const storySchema = new Schema({
+  userId: String,
+  messageId: String,
+  sentence: String,
+  nouns: Array
+});
+
+module.exports = mongoose.model('story', storySchema);
--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 require('./config/express')(app);
 require('./config/passport')(app);
 require('./routes')(app);
 
-app.listen(app.get('port'), function appListener() {
-  console.log('Node app is running at localhost:' + app.get('port'));
+// mongoose.connect('mongodb://localhost/storyteller', {});
+mongoose.connect('mongodb://storyteller:storyteller@ds151707.mlab.com:51707/storyteller', {});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
