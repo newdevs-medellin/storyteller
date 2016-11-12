59b7874665d83a02f407233392fdc53e8b7f5377
camilo montoya <camilomontoya2@gmail.com>
2016-11-12 10:03:09 -0500

    PR #4 : express handlebars config
    * partyparrot
    
    * parrot gif
    
    * handlebars init config

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var express = require('express')
-var app = express()
+'use strict';
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.use('/assets', express.static('assets'));
 
-app.get('/', function(request, response) {
-  response.send('Hello manniggas!')
-})
+app.get('/', function(req, res) {
+  res.render('hello');
+});
+
+
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+  console.log("Node app is running at localhost:" + app.get('port'));
 })
