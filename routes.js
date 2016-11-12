a4461c70659e75fe8a54250c7e9f8adedb7dce9a
Pablo Velásquez <pablovem@users.noreply.github.com>
2016-11-12 14:25:07 -0500

    Fix route issue (#10)
    Update template in routes.

--- routes.js
+++ routes.js
@@ -4,6 +4,6 @@ module.exports = function (app) {
   app.use('/api/v1/story', require('./api/story'));
 
   app.get('/', function(req, res) {
-    res.render('hello');
+    res.render('home');
   });
 };
