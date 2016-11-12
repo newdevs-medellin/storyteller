5f417b7167d17340ef56fb45e705520bdb2b3e59
camilo montoya <camilomontoya2@gmail.com>
2016-11-12 10:56:56 -0500

    PR #7 : Feature/eslint config
    * PR #5 : express handlebars config
    
    * partyparrot
    
    * parrot gif
    
    * handlebars init config
    
    * eslintrc file
    
    * eslint devDependencies and eslintConfig on package.json
    
    * deleting additional public dir
    
    * eslint script in package.json
    
    * index.js eslint errors
    
    * adding eslintignore
    
    * eslintrc file
    
    * eslint devDependencies and eslintConfig on package.json
    
    * deleting additional public dir
    
    * eslint script in package.json
    
    * index.js eslint errors
    
    * adding eslintignore

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.set('view engine', 'handlebars');
 
 app.use('/assets', express.static('assets'));
 
-app.get('/', function(req, res) {
+app.get('/', function expressProcessReq(req, res) {
   res.render('hello');
 });
 
-app.set('port', (process.env.PORT || 5000));
+app.set('port', process.env.PORT || 5000);
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'));
-})
+app.listen(app.get('port'), function appListening() {
+  console.log('Node app is running at localhost:' + app.get('port'));
+});
