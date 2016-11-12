044e2dba99482af49022664138170e0849705385
camilo <juancamilo@adviceinteractive.com>
2016-11-11 23:11:11 -0500

    parrot gif

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('<img src="http://cultofthepartyparrot.com/parrots/partyparrot.gif">')
+  response.send('<img src="/partyparrot.gif">')
 })
 
 app.listen(app.get('port'), function() {
