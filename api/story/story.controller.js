e006cd63ccf5fb505169143bd4b54c79d577708f
Jesse cogollo <cogollo87@gmail.com>
2016-11-12 17:49:26 -0500

    Feature/schema mongodb (#15) (#16)
    * create story model and service
    
    * conf mongodb local to save and get stories
    
    * adding connection mongodb to 3party service mlab

--- api/story/story.controller.js
+++ api/story/story.controller.js
@@ -1,13 +1,27 @@
 'use strict';
+const storyModel = require('./story.model');
 
-exports.post = function postRequestHandling(req, res) {
-  return res.status(200).json({post: true});
+const handleError = function handleError(err, res) {
+  return res.status(400).json({error: err});
 };
 
-exports.getById = function getRequestHandling(req, res) {
-  return res.status(200).json({getById: true});
+exports.post = function (req, res) {
+  storyModel.create(req.body, function (err, data) {
+    if (err) return handleError(err, res);
+    return res.status(200).json({data: data});
+  });
 };
 
-exports.get = function listRequestHandling(req, res) {
-  return res.status(200).json({get: true});
+exports.getById = function (req, res) {
+  storyModel.findById(req.params.id, function (err, story) {
+    if (err) return handleError(err, res);
+    return res.status(200).json({data: story});
+  });
+};
+
+exports.get = function (req, res) {
+  storyModel.find({}, function (err, stories) {
+    if (err) return handleError(err, res);
+    return res.status(200).json({data: stories});
+  });
 };
