bd07774bc5287b01e8cb5e84f52b0b918d5d631b
camilo <juancamilo@adviceinteractive.com>
2016-11-11 23:01:18 -0500

    partyparrot

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello manniggas!')
+  response.send('<img src="http://cultofthepartyparrot.com/parrots/partyparrot.gif">')
 })
 
 app.listen(app.get('port'), function() {
