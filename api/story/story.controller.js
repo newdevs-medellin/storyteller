74637f4130986eb3757f92ddab2183a4a9d74133
Pablo Velásquez <pablovem@users.noreply.github.com>
2016-11-13 16:10:06 -0500

    Remove nlp (#21)

--- api/story/story.controller.js
+++ api/story/story.controller.js
@@ -1,6 +1,4 @@
 'use strict';
-const ai = require('apiai');
-const agent = ai("9f37a89a1b504474b2704c16498a4c60");
 const storyModel = require('./story.model');
 
 const handleError = function handleError(err, res) {
@@ -8,29 +6,10 @@ const handleError = function handleError(err, res) {
 };
 
 exports.post = function (req, res) {
-  let data = req.body;
-  data.nouns = [];
-  // Process sentence
-  let nlp = agent.textRequest(data.sentence);
-  nlp.on('response', function(response) {
-    // Character is defined in first sentence
-    if(response.result.action === 'p1'){
-      // Main Character
-      let character = response.result.parameters.character;
-      data.nouns.push({'character': character});
-    }
-
-    storyModel.create(data, function (err, data) {
-      if (err) return handleError(err, res);
-      return res.status(200).json({data: data});
-    });
-
+  storyModel.create(req.body, function (err, data) {
+    if (err) return handleError(err, res);
+    return res.status(200).json({data: data});
   });
-  nlp.on('error', function(error) {
-    console.log(error);
-    return res.status(500).json({error: error});
-  });
-  nlp.end();
 };
 
 exports.getById = function (req, res) {
