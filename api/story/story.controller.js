80892e3a1f62220fed44733a55bcb6afa5ed722b
Jesse cogollo <cogollo87@gmail.com>
2016-11-13 18:00:34 -0500

    Feature/api giphy (#23)
    * create story model and service
    
    * conf mongodb local to save and get stories
    
    * adding connection mongodb to 3party service mlab
    
    * get the sentence form voice and save it
    
    * change name of variable
    
    * get images from giphy done

--- api/story/story.controller.js
+++ api/story/story.controller.js
@@ -1,14 +1,27 @@
 'use strict';
 const storyModel = require('./story.model');
+const request = require('request');
+const Q = require('q');
 
 const handleError = function handleError(err, res) {
   return res.status(400).json({error: err});
 };
 
 exports.post = function (req, res) {
-  storyModel.create(req.body, function (err, data) {
-    if (err) return handleError(err, res);
-    return res.status(200).json({data: data});
+  Promise.all(req.body.sentence.split(' ').map(function (word) {
+    return getGif(word).then(function (value) {
+      return {noun: word, imgUrl: value};
+    });
+  })).then(function (result) {
+    req.body.nouns = result;
+    storyModel.create(req.body, function (err, data) {
+      if (err) return handleError(err, res);
+      console.log('req.body', req.body);
+      return res.status(200).json({data: data});
+     });
+  }, function (reason) {
+    console.log('reason', reason);
+    return reason;
   });
 };
 
@@ -24,4 +37,17 @@ exports.get = function (req, res) {
     if (err) return handleError(err, res);
     return res.status(200).json({data: stories});
   });
+};
+
+function getGif (word) {
+  let deferred = Q.defer();
+  request.get(`http://api.giphy.com/v1/gifs/search?q=${word}&limit=1&api_key=dc6zaTOxFJmzC`, function (error, response, body) {
+    let bodyObj = JSON.parse(body);
+    if(bodyObj.data.length) {
+      deferred.resolve(bodyObj.data[0].embed_url);
+    }else{
+      deferred.resolve('');
+    }
+  });
+  return deferred.promise;
 };
